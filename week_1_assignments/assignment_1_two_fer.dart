2f4bb9d67ed120281e663129cf563de21f538b89
Anthony Bekoe Bankah <107210739+bankah-junior@users.noreply.github.com>
2025-07-29 10:07:35 +0000

    Update assignment_1_two_fer.dart

--- week_1_assignments/assignment_1_two_fer.dart
+++ week_1_assignments/assignment_1_two_fer.dart
@@ -1,23 +1,13 @@
 import 'dart:io';
 
 void main() {
-  // Prompt the user to enter their name
   print("Please enter the person's name: ");
-
-  // Read the input from the console
   String? personName = stdin.readLineSync();
-
-  // Call the twoFer function with the provided name and store the result
   String twoFerMessage = twoFer(personName);
-
-  // Print the result
   print(twoFerMessage);
 }
 
 String twoFer(String? name) {
-  // If the name is null or empty, use "you" else use the name provided
   String person = (name == null || name.isEmpty) ? 'you' : name.trim();
-
-  // Return message
   return '\nOne for $person, one for me.';
 }
