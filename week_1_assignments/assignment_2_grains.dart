613e092b618aef80e49416623b46dce99ddfcbe5
Anthony Bekoe Bankah <107210739+bankah-junior@users.noreply.github.com>
2025-07-29 10:14:29 +0000

    Update assignment_2_grains.dart

--- week_1_assignments/assignment_2_grains.dart
+++ week_1_assignments/assignment_2_grains.dart
@@ -1,23 +1,9 @@
-import 'dart:io';
-
 void main() {
-  print('Please enter a number from 1 to 64: ');
-  String? number = stdin.readLineSync();
-  if (number != null && number.isNotEmpty) {
-    int? n = int.tryParse(number);
-    if (n != null && n >= 1 && n <= 64) {
-      BigInt result = square(n);
+  int n = 5;
+  BigInt result = square(n);
       print('\nThe number of grains on square $n is: $result');
       BigInt totalGrains = total();
       print('\nThe total number of grains on the chessboard is: $totalGrains');
-    } else {
-      print('\nPlease enter a valid number between 1 and 64.');
-    }
-  } else {
-    print(
-      '\nInput cannot be null or empty. Please enter a number between 1 and 64.',
-    );
-  }
 }
 
 BigInt square(final int n) {
